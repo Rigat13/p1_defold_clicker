eeb4e4b6b88772a557a0d74424b8520ebe634a16
Anass Arrahi Arrai <82566327+Anass-Arrahi-Arrai@users.noreply.github.com>
2023-02-05 21:47:16 +0100

    Sprites_Animats: Implementats
    Sprites autoclickers Carles i Anass Implementats.

--- aarrahi_crigat_p1_defold/main/assets/scripts/autoclicker/autoclicker1.go
+++ aarrahi_crigat_p1_defold/main/assets/scripts/autoclicker/autoclicker1.go
@@ -21,8 +21,8 @@ components {
 embedded_components {
   id: "autoclicker_sprite"
   type: "sprite"
-  data: "tile_set: \"/main/assets/textures/autoclicker_texture_1.atlas\"\n"
-  "default_animation: \"autoclicker_texture_1\"\n"
+  data: "tile_set: \"/main/assets/Sprites/sprite_animation_Anass.atlas\"\n"
+  "default_animation: \"Anass_idle\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -38,8 +38,8 @@ embedded_components {
     w: 1.0
   }
   scale {
-    x: 0.1
-    y: 0.1
-    z: 0.1
+    x: 0.166
+    y: 0.166
+    z: 0.166
   }
 }
